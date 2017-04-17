ba6ccb972ce72deb4ea3930e899987106cdefccc
techanda <timramsier@gmail.com>
2017-04-17 09:55:54 -0700

    Add "more content" indicator at bottom of welcome page
    * Add a down arrow to notify user that there is more content at end of welcome animation

--- js/Jumbotron.js
+++ js/Jumbotron.js
@@ -7,7 +7,33 @@ const Jumbo = React.createClass({
     header: string,
     message: string
   },
+  welcomeAnimation (selector, interval, callback) {
+    window.setTimeout(() => {
+      if (document.querySelectorAll(`${selector}.is-paused`).length > 0) {
+        document.querySelectorAll(`${selector}.is-paused`)[0].classList.remove('is-paused')
+        this.welcomeAnimation(selector, interval)
+      } else {
+        document.querySelectorAll('.letter.expanded-letter').forEach((element) => {
+          window.setTimeout(() => {
+            element.classList.remove('expanded-letter')
+            window.setTimeout(() => {
+              document.querySelectorAll('.jumbotron p').forEach((element) => {
+                element.style.opacity = '1.0'
+              })
+              window.setTimeout(() => {
+                document.querySelector('.downarrow img').style.opacity = 0.6
+              }, 1200)
+            }, 1200)
+          }, interval * 5)
+        })
+      }
+    }, interval)
+  },
   componentDidMount () {
+    // animation for welcome text of hero section
+    const welcomeAnimation = this.welcomeAnimation
+
+    // prepare header for animation by seperating the letters
     var message = this.props.header
     var letters = message.split('').map((letter) => {
       let empty
@@ -20,31 +46,26 @@ const Jumbo = React.createClass({
         `<span class='letter expanded-letter ${empty} is-paused'>${letter}</span>`
       )
     })
-    var animation = `<div class='animation'>${letters.join('')}</div>`
-    document.querySelector('.hello').innerHTML = animation
 
+    // render prepared letters to the DOM
+    var animation = `<div class='animation'>${letters.join('')}</div>`
+    document.querySelector('.header').innerHTML = animation
+
+    // animate welcome section on page load
     window.addEventListener('load', () => {
       welcomeAnimation('.letter', 400)
     })
-    const welcomeAnimation = (selector, interval, callback) => {
-      window.setTimeout(() => {
-        if (document.querySelectorAll(`${selector}.is-paused`).length > 0) {
-          document.querySelectorAll(`${selector}.is-paused`)[0].classList.remove('is-paused')
-          welcomeAnimation(selector, interval)
-        } else {
-          document.querySelectorAll('.letter.expanded-letter').forEach((element) => {
-            window.setTimeout(() => {
-              element.classList.remove('expanded-letter')
-              window.setTimeout(() => {
-                document.querySelectorAll('.jumbotron p').forEach((element) => {
-                  element.style.opacity = '1.0'
-                })
-              }, 1200)
-            }, interval * 5)
-          })
-        }
-      }, interval)
-    }
+
+    window.addEventListener('scroll', (event) => {
+      // var breakRatio = window.innerWidth < 768 ? 2 : 1.25
+      var element = document.querySelector('.downarrow img')
+      var parent = element.parentElement
+      var top = parent.getBoundingClientRect().top
+
+      if (top <= 150) {
+        element.style.opacity = 0.0
+      }
+    })
   },
   render () {
     const { message } = this.props
@@ -52,11 +73,14 @@ const Jumbo = React.createClass({
       <div className='container'>
         <div className='row'>
           <Jumbotron>
-            <h1><div className='hello' /></h1>
+            <h1><div className='header' /></h1>
             <p>
               {message}
             </p>
           </Jumbotron>
+          <div className='downarrow'>
+            <img src='/public/img/downTriangle.png' />
+          </div>
         </div>
       </div>
     )
